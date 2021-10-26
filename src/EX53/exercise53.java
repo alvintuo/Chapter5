ed7099b225a21351c636f7df9e7cb6d07af333b6
Alvin <52668092+alvintuo@users.noreply.github.com>
2021-10-26 14:29:04 -0400

    New Chapter 5.3

---
+++ src/EX53/exercise53.java
@@ -0,0 +1,38 @@
+package EX53;
+import java.util.Scanner;
+
+/*
+    Exercise 5.3 Prompt
+    Design and implement an application that determines and prints the number of odd, even,
+    and zero digits in an integer value read from the keyboard.
+ */
+public class exercise53 {
+
+    public static void main(String[] args) {
+        int num, len = 0, index = 0;
+
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+
+        String number = new String(Integer.toString(num));
+        len = number.length();
+
+        while (index < len) {
+            String tempNum = number.substring(index, index+1);
+            int i = Integer.parseInt(tempNum);
+            if (((i % 2) == 0) && !(i == 0)) {
+                System.out.println("The number at index " + index + " is even.");
+                index++;
+            }
+            else if (((i % 2) - 1) == 0){
+                System.out.println("The number at index " + index + " is odd.");
+                index++;
+            }
+            else {
+                System.out.println("The number at index " + index + " is zero");
+                index++;
+            }
+        }
+
+    }
+}
